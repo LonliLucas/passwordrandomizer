589e18008de6431d12f9ab8fd84e4e52f80df941
LonliLucas <lukej.gilless@gmail.com>
2023-09-14 13:21:05 -0600

    Removed Console.log lines and updated the README

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,8 @@ var special = `"!@#$%^&*()-_=+[]{}|;:/?.>'`
 
 // Write password to the #password input
 function generatePassword() {
-  var passwordLength = prompt("Enter a length between 8 and 16")
-  if(passwordLength < 8 || passwordLength > 16 || isNaN(passwordLength)) {
+  var passwordLength = prompt("Enter a length between 10 and 20")
+  if(passwordLength < 10 || passwordLength > 20 || isNaN(passwordLength)) {
     alert("Please enter a valid password length")
     generatePassword()
     return
@@ -18,35 +18,29 @@ function generatePassword() {
   var includeNumbers = confirm("Include numbers?")
   var includeSpecial = confirm("Include special characers such as $ and @?")
   
-  console.log(passwordLength, includeUppercase, includeLowercase, includeNumbers,includeSpecial)
   var possibleCharacters = ""
 
   if(includeUppercase){
-    console.log("Yep1")
     possibleCharacters += uppercase
   }
   if(includeLowercase){
-    console.log("Yep2")
     possibleCharacters += lowercase
   }
   if(includeNumbers){
-    console.log("Yep3")
     possibleCharacters += numbers
   }
   if(includeSpecial){
-    console.log("Yep4")
     possibleCharacters += special
   }
   if(!includeUppercase && !includeLowercase && !includeSpecial && !includeLowercase){
     alert("You must pick at least one category of Characters.")
   }
-  console.log(possibleCharacters)
   var password = ""
   for(var i = 0; i < passwordLength; i++){
     var randomIndex = Math.floor(Math.random()*(possibleCharacters.length-1))
     password += possibleCharacters[randomIndex]
   }
-  console.log(password)
+
   return password
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # passwordrandomizer
+
+## Description
+
+A password creation tool that will prompt for options for password creation, then will generate a random password based on your selected criteria.
+
+## Usage
+
+Click the red button labelled "Generate Password" then answer the prompts to choose criteria to create a password. Enter a password length between 10 and 20, then select Ok to include what the prompt asks and Cancel to choose not to include it.
